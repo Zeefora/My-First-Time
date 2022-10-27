3cf3bf676791b2719c0b3d0e9f51e2630129032b
Zeefora <116573371+Zeefora@users.noreply.github.com>
2022-10-27 16:28:03 +0800

    my first python code!

---
+++ Me.py
@@ -0,0 +1,7 @@
+"""
+Semua sintaksis dasar bahasa pemograman dari :
+1. Sekuensial : Langkah berurutan
+2. Percabangan : Langkah melompat jika terpenuhi
+3. Perulangan : Mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
+"""
+print("Ibu, Berkata")
