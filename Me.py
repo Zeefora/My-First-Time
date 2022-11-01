685cce7d6bcdddf7b81089af541d038ddda58f42
Zeefora <116573371+Zeefora@users.noreply.github.com>
2022-11-01 17:19:30 +0800

    Done Well!!!

--- Me.py
+++ Me.py
@@ -4,4 +4,10 @@ Semua sintaksis dasar bahasa pemograman dari :
 2. Percabangan : Langkah melompat jika terpenuhi
 3. Perulangan : Mengulang langkah yang sama berkali-kali selama/sampai kondisi terpenuhi
 """
-print("Ibu, Berkata")
+
+# Sekuensial
+print('Ibu, Berkata :"Nak pergilah ke toko"')
+print('Budi Menjawab :"Baik,apa yang harus saya lakukan di toko?"')
+print('Ibu Menjawab :"Beli 1 botol susu,dan jika ada telur beli 6 butir telur"')
+print("Maka Budi pergi ke toko")
+print("Dan mulai berbelanja")
