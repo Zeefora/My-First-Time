199e157f03df4c3c012df09e2b0b5d1758a69ecf
Zeefora <116573371+Zeefora@users.noreply.github.com>
2022-12-08 09:33:11 +0800

    Done Well!!!

--- Me.py
+++ Me.py
@@ -11,3 +11,21 @@ print('Budi Menjawab :"Baik,apa yang harus saya lakukan di toko?"')
 print('Ibu Menjawab :"Beli 1 botol susu,dan jika ada telur beli 6 butir telur"')
 print("Maka Budi pergi ke toko")
 print("Dan mulai berbelanja")
+
+# Percabangan
+jumlah_botol_susu = 15
+jumlah_telur = 20
+print(f"Jumlah botol susu {jumlah_botol_susu} btl")
+print(f"Jumlah telur {jumlah_telur} butir")
+
+if jumlah_botol_susu > 0:
+    print("Budi mengecek harganya,dan ternyata uangnya cukup")
+    if jumlah_telur == 0:
+        print("Budi membeli 1 botol susu")
+    else:
+        print("Budi membeli 6 botol susu")
+else:
+    print("Budi tidak jadi membeli 1 botol susu")
+
+print("Budi pulang ke rumah")
+print("Menyampaikan hasilnya kepada ibu")
